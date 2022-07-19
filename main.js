72d73e57c0a399894cbf2aa6d5b660f5f947feb7
Yutaro Yoshii <yakumodanshi@gmail.com>
2022-07-19 20:54:52 +0900

    add router and edit page

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ class Data {
                 // Create an objectStore for this database
                 const metaStore = db.createObjectStore("essay-metadata", {
                     keyPath: "id",
-                    autoIncrement:true
+                    autoIncrement: true
                 });
 
                 metaStore.createIndex("tags", "tags", {
@@ -56,9 +56,9 @@ class Data {
                 metaStore.createIndex("opened", "timestamps.opened", {
                     unique: false
                 });
-                
+
                 const bodyStore = db.createObjectStore("essay-body", {
-                    keypath: "id"
+                    keyPath: "id"
                 });
             });
 
@@ -78,14 +78,14 @@ class Data {
         console.log(db);
     }
     order_vals = {
-        "modify-new":["modified","prev"],
-        "modify-old":["modified","next"],
-        "opened-new":["opened","prev"],
-        "opened-old":["opened","next"],
-        "create-new":["created","prev"],
-        "create-old":["created","next"],
-        "alph-a-z":["title","next"],
-        "alph-z-a":["title","prev"]
+        "modify-new": ["modified", "prev"],
+        "modify-old": ["modified", "next"],
+        "opened-new": ["opened", "prev"],
+        "opened-old": ["opened", "next"],
+        "create-new": ["created", "prev"],
+        "create-old": ["created", "next"],
+        "alph-a-z": ["title", "next"],
+        "alph-z-a": ["title", "prev"]
     };
     async search(params) {
         let that = this;
@@ -98,21 +98,21 @@ class Data {
         let {
             db
         } = this;
-        
+
         search_str = search_str.toLowerCase();
-        
+
         const transaction = db.transaction(["essay-metadata"], "readonly");
         const metaStore = transaction.objectStore("essay-metadata");
         //wait for the iteration to be over
         let results = [];
         await new Promise((res, rej) => {
-            let [index_str,direction] = that.order_vals[order];
-            console.log(index_str,direction);
-            metaStore.index(index_str).openCursor(null,direction).onsuccess = function(e) {
+            let [index_str, direction] = that.order_vals[order];
+            console.log(index_str, direction);
+            metaStore.index(index_str).openCursor(null, direction).onsuccess = function(e) {
                 let cursor = e.target.result;
                 if (cursor) {
                     let val = cursor.value;
-                    if(val.title.toLowerCase().match(search_str)){
+                    if (val.title.toLowerCase().match(search_str)) {
                         results.push(val);
                     }
                     cursor.continue();
@@ -125,7 +125,7 @@ class Data {
         await Pause(100);
         console.log(results);
         return results;
-    
+
 
         //dummy return value
         /*return makearr(1512).map(_ => {
@@ -135,53 +135,146 @@ class Data {
             }
         });*/
     }
-    async createNewEntry(){
-        const {db} = this;
+    async createNewEntry() {
+        const {
+            db
+        } = this;
         let i = 1;
         //getting the available names
-        while(true){
+        while (true) {
             const transaction = db.transaction(["essay-metadata"], "readonly");
             const metaStore = transaction.objectStore("essay-metadata");
-            let cnt = await new Promise((res,rej)=>{
-                metaStore.index("title").count(`untitled ${i}`).onsuccess = function(e){
+            let cnt = await new Promise((res, rej) => {
+                metaStore.index("title").count(`untitled ${i}`).onsuccess = function(e) {
                     res(e.target.result);
                 };
             });
-            //auto-commits, so no need to .commit();
-            if(cnt === 0){
+            if (cnt === 0) {
                 break;
             }
             i++;
+            //auto-commits, so no need to .commit();, but just in case
+            transaction.commit();
         }
         let title = `untitled ${i}`;
         console.log(title);
         let tsnow = new Date(Date.now());
-        
+
         let meta = {
             title,
-            body:"",
-            timestamps:{
-                created:tsnow,
-                modified:tsnow,
-                opened:tsnow,
+            body: "",
+            timestamps: {
+                created: tsnow,
+                modified: tsnow,
+                opened: tsnow,
             },
-            tags:[]
+            tags: []
         };
-        
-        const create_tx = db.transaction(["essay-metadata"],"readwrite");
-        const metastore = create_tx.objectStore("essay-metadata");
-        const val = await new Promise((res,rej)=>{
+
+        const transaction = db.transaction(["essay-metadata","essay-body"], "readwrite");
+        const metastore = transaction.objectStore("essay-metadata");
+        const id = await new Promise((res, rej) => {
             //IDBRequest
-            metastore.add(meta).onsuccess = function(e){
-                console.log(e);
-                res(e);
+            metastore.add(meta).onsuccess = function(e) {
+                //results in the generated key (id)
+                res(e.target.result);
             };
         });
-        return val;
+        
+        
+        let body = {
+            id,
+            body:""
+        };
+        const bodystore = transaction.objectStore("essay-body");
+        await new Promise((res, rej) => {
+            //IDBRequest
+            bodystore.add(body).onsuccess = function(e) {
+                //results in the generated key (id)
+                res(e.target.result);
+            };
+        });
+        return id;
+    }
+    async getEntry(id) {
+        const {db} = this;
+        //returns data and metadata
+        const transaction = db.transaction(["essay-metadata", "essay-body"], "readonly");
+        let [metadata,body] = await Promise.all([
+            new Promise((res, rej) => {
+                transaction.objectStore("essay-metadata")
+                .get(id).onsuccess = function(e) {
+                    res(e.target.result);
+                };
+            }),
+            new Promise((res, rej) => {
+                transaction.objectStore("essay-body")
+                .get(id).onsuccess = function(e) {
+                    res(e.target.result);
+                };
+            }),
+        ]);
+        return [metadata,body];
+    }
+    async saveEntryBody(id, body) {
+        const {db} = this;
+        const transaction = db.transaction(["essay-body"], "readwrite");
+        const bodystore = transaction.objectStore("essay-body");
+        await new Promise((res, rej) => {
+            //IDBRequest
+            bodystore.put(body).onsuccess = function(e) {
+                //results in the generated key (id)
+                res(e.target.result);
+            };
+        });
+    }
+    async saveEntryMetadata(id, meta) {
+        const {db} = this;
+        const transaction = db.transaction(["essay-metadata"], "readwrite");
+        const metastore = transaction.objectStore("essay-metadata");
+        await new Promise((res, rej) => {
+            //IDBRequest
+            metastore.put(meta).onsuccess = function(e) {
+                //results in the generated key (id)
+                res(e.target.result);
+            };
+        });
+    }
+    async deleteRecord(id){
+        const {db} = this;
+        //returns data and metadata
+        const transaction = db.transaction(["essay-metadata", "essay-body"], "readwrite");
+        await Promise.all([
+            new Promise((res, rej) => {
+                transaction.objectStore("essay-metadata")
+                .delete(id).onsuccess = function(e) {
+                    res(e.target.result);
+                };
+            }),
+            new Promise((res, rej) => {
+                transaction.objectStore("essay-body")
+                .delete(id).onsuccess = function(e) {
+                    res(e.target.result);
+                };
+            }),
+        ]);
+        console.log("successfully deleted id "+id);
+        return id;
     }
 };
 
 
+
+
+
+class EvtELEM extends ELEM{
+    constructor(){
+        super(...arguments);
+        let bus = new Events();
+        this.on = bus.on.bind(this);
+        this.emit = bus.emit.bind(this);
+    }
+}
 
 
 
@@ -268,15 +361,11 @@ class Select extends ELEM {
 };
 
 
-class SearchBar extends ELEM {
+class SearchBar extends EvtELEM {
     constructor(data) {
         super("div", "class:searchbar");
         let that = this;
         this.data = data;
-
-        let bus = new Events();
-        this.on = bus.on.bind(this);
-        this.emit = bus.emit.bind(this);
 
         let input = this.add("input", "type:text;class:bar;");
         this.input = input;
@@ -303,13 +392,13 @@ class SearchBar extends ELEM {
                 let r = l.split(";");
                 return [r[0].trim(), r[1].trim()];
             })));
-        this.select.on("input",()=>{
+        this.select.on("input", () => {
             that.handleInput();
         });
-        
-        let createNew = this.add("div","class:newbtn","+new");
-        
-        createNew.on("click",()=>{
+
+        let createNew = this.add("div", "class:newbtn", "+new");
+
+        createNew.on("click", () => {
             that.emit("create-new");
         });
     }
@@ -409,11 +498,13 @@ class PageSelect extends ELEM {
         this.numbers = this.add("div");
         this.add(new this.btn(">", () => wrapper.emit(">")));
         this.add(new this.btn(">>", () => wrapper.emit(">>")));
+        //wrapper will be deleted at teh same time as this calss
+        //thus garbage collection not needed even being an external callback
         wrapper.on("setPage", this.setPage.bind(this));
-        wrapper.on("stateChange",(state)=>{
-            if(state === "results"){
+        wrapper.on("stateChange", (state) => {
+            if (state === "results") {
                 this.e.style.display = "block";
-            }else{
+            } else {
                 this.e.style.display = "none";
             }
         });
@@ -466,6 +557,7 @@ Style += /*css*/ `
     background-color:#eee;
     border-radius:5px;
     padding:10px;
+    min-height:200px;
 }
 
 .boxes .box:hover{
@@ -484,33 +576,31 @@ Style += /*css*/ `
 
 `;
 
-class ResultBoxes extends ELEM {
-    constructor() {
+class ResultBoxes extends EvtELEM {
+    constructor(app) {
         super("div", "class:boxes");
         let that = this;
-        let bus = new Events();
-        this.on = bus.on.bind(this);
-        this.emit = bus.emit.bind(this);
-        
+        this.app = app;
+
         //messages
-        let no_r_msg = this.add("div","class:no-result-msg","No result found");
-        let searching_msg = this.add("div","class:searching-msg","Searching...");
-        
+        let no_r_msg = this.add("div", "class:no-result-msg", "No result found");
+        let searching_msg = this.add("div", "class:searching-msg", "Searching...");
+
         this.add(new PageSelect(this));
         this.boxes = this.add("div", "class:container");
         this.add(new PageSelect(this));
         this.on("setPage", this.setPage);
-        
+
         //display states
-        this.on("stateChange",(state)=>{
-            no_r_msg.e.style.display = 
+        this.on("stateChange", (state) => {
+            no_r_msg.e.style.display =
                 state === "no-results" ? "block" : "none";
-            searching_msg.e.style.display = 
+            searching_msg.e.style.display =
                 state === "searching" ? "block" : "none";
-            this.boxes.e.style.display = 
+            this.boxes.e.style.display =
                 state === "results" ? "grid" : "none";
         });
-        
+
     }
     page = 1; //1st page
     max = 50;
@@ -521,6 +611,7 @@ class ResultBoxes extends ELEM {
         this.emit("setPage", 1);
     }
     setPage(n) {
+        let {app} = this;
         //remove all the children
         this.boxes.children.foreach(c => c.remove());
         //slice the section
@@ -532,33 +623,34 @@ class ResultBoxes extends ELEM {
             let box = this.boxes.add("div", "class:box");
             box.add("h2", 0, data.title);
             box.add("p", 0, data.body);
+            box.on("click",()=>app.openEditor(data.id));
         }
     }
     state_val = "initializing";
-    get state(){
+    get state() {
         return this.state_val;
     }
-    set state(val){
+    set state(val) {
         this.state_val = val;
-        this.emit("stateChange",val);
+        this.emit("stateChange", val);
     }
 };
 
 
 
 class SearchWidget extends ELEM {
-    constructor(database) {
+    constructor(database, app) {
         super("div");
         let bar = this.add(new SearchBar());
-        let rbox = this.add(new ResultBoxes());
+        let rbox = this.add(new ResultBoxes(app));
         //setup the initial state
         //simulate input
-        (async function(){
+        (async function() {
             bar.handleInput();
             let data = await database.search(bar.params);
-            if(data.length === 0){
+            if (data.length === 0) {
                 rbox.state = "no-results";
-            }else{
+            } else {
                 rbox.state = "results";
                 rbox.update(data);
             }
@@ -566,28 +658,184 @@ class SearchWidget extends ELEM {
         bar.on("search", async () => {
             rbox.state = "searching";
             let data = await database.search(bar.params);
-            if(data.length === 0){
+            if (data.length === 0) {
                 rbox.state = "no-results";
-            }else{
+            } else {
                 rbox.state = "results";
                 rbox.update(data);
             }
         });
-        bar.on("create-new",async ()=>{
+        bar.on("create-new", async () => {
             console.log("creating a new entry");
             let id = await database.createNewEntry();
+            console.log(id);
+            app.openEditor(id);
         });
+    }
+    destroy(){
+        //remove events etc
+        this.remove();
+    }
+};
+
+
+
+Style += `
+.editor{
+    width:100%;
+    height:100%;
+}
+
+.editor .searchbar{
+    justify-content: left;
+}
+
+.editor .searchbar>*{
+    display:inline-block;
+}
+
+.editor .searchbar>.title,
+.editor .searchbar>.delete-btn,
+.editor .searchbar>.save-btn{
+    border:solid 1px #444;
+    text-align:center;
+    background-color:#eee;
+}
+
+.editor .searchbar>.delete-btn{
+    margin-right:5px;
+    background-color:#ff896e;
+    color:#000;
+}
+
+.editor .searchbar>.save-btn{
+    background-color:#6eff6e;
+    color:#000;
+}
+
+.editor textarea{
+    width:100%;
+    height:calc(100vh - 63px);
+}
+
+`;
+
+class Editor extends ELEM {
+    Title = class extends EvtELEM{
+        constructor(name){
+            super("div","class:title;contenteditable:true;",name);
+            let that = this;
+            super.on("keydown",e=>{
+                if(e.key !== "Enter")return;
+                e.preventDefault();
+                that.e.blur();
+            });
+            super.on("focusout",e=>{
+                that.emit("enter");
+            });
+        }
+        get value(){
+            return this.e.textContent;
+        }
+    }
+    constructor(database, app, id) {
+        super("div","class:editor");
+        this.database = database;
+        this.app = app;
+        this.id = id;
+        this.initialize();
+    }
+    foreignListeners = [];
+    async initialize(){
+        let {database, app, id} = this;
+        let that = this;
+        let [metadata,body] = await database.getEntry(id);
+        console.log(metadata,body);
+        this.metadata = metadata;
+        this.body = body;
+        let head = this.add("div","class:searchbar;");
+        this.title = head.add(new this.Title(metadata.title));
+        this.title.on("enter",async ()=>{
+            console.log("entered");
+            that.save();
+        });
+        head.add("div",0,0,"flex-grow:1;");
+        head.add("div","class:delete-btn","Delete Document").on("click",()=>{
+            database.deleteRecord(id);
+            //could await, but don't because it's safe anyway bc it's transaction
+            app.openHome();
+        });
+        head.add("div","class:save-btn","Save and exit").on("click",()=>{
+            that.save();
+            app.openHome();
+        });
+        
+        let editor_body = this.add("div","class:body;");
+        this.textarea = editor_body.add("textarea");
+        this.textarea.e.value = body.body;
+        
+        this.foreignListeners.push(app.on("save",async ()=>{
+            that.save();
+        }));
+    }
+    async save(){
+        let {database, app, id, body, metadata,textarea,title} = this;
+        console.log("saving");
+        body.body = textarea.e.value;
+        metadata.title = title.value;
+        metadata.body = body.body.slice(0,200);
+        await database.saveEntryMetadata(id,metadata);
+        await database.saveEntryBody(id,body);
+        console.log("saved");
+    }
+    async destroy(){
+        this.foreignListeners.map(l=>l.remove());
+        this.remove();
+    }
+};
+
+
+class EssayEditApp extends EvtELEM {
+    active = null;
+    constructor(parent) {
+        super(parent);
+        let that = this;
+        //registering all the shortcuts
+        window.addEventListener("keydown",(e)=>{
+            
+            if(!(e.shiftKey || e.ctrlKey || e.metaKey))return;
+            if(e.key === "s"){
+                e.preventDefault();
+                that.emit("save");
+            }
+        });
+        
+        
+        
+        this.add("style", 0, Style);
+        this.initialize();
+        
+    }
+    async initialize() {
+        let database = new Data();
+        await database.initialize();
+        this.database = database;
+        this.openHome();
+    }
+    openEditor(id) {
+        if (this.active)this.active.destroy();
+        this.active = this.add(new Editor(this.database, this, id));
+    }
+    openHome() {
+        if (this.active) this.active.destroy();
+        this.active = this.add(new SearchWidget(this.database, this));
     }
 };
 
 
 let main = async function() {
     //return;
-    let body = new ELEM(document.body);
-    body.add("style", 0, Style);
-    let database = new Data();
-    await database.initialize();
-    body.add(new SearchWidget(database));
+    let editorApp = new EssayEditApp(document.body);
 };
 
 main();
